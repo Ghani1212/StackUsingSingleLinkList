8896bbc577c6560564100bc782c305101635bd62
Ghani Zumar <ghani.zumar.ft21@mail.umy.ac.id>
2022-11-28 16:19:04 +0700

    Add project files.

---
+++ StackUsingSingleLinkList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackUsingSingleLinkList
+{
+    class node
+    {
+        public int info;
+        public node next;
+        public node (int i, node n)
+        {
+            info = i; 
+            next = n; 
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
